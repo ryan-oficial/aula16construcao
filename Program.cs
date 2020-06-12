a851be5a99115a05fbd8200c35e6ed0b233b9e4e
ryan <ryanferraz.barbosa@gmail.com>
2020-06-12 16:50:32 -0300

    terminado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula16Construçao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto sapato = new Produto();
+
+            Produto chinelo = new Produto(789);
+            System.Console.WriteLine(chinelo.Codigo);
+
+            System.Console.WriteLine();
+
+            Produto tenis = new Produto(678, "All Star", "Couro", 45);
+            System.Console.WriteLine(tenis.Codigo);
+            System.Console.WriteLine(tenis.Nome);
+            System.Console.WriteLine(tenis.Descricao);
+            System.Console.WriteLine(tenis.Estoque);
+        }
+    }
+}
